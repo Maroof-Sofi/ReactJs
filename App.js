99f63a36eb76a887149ae794ef0a1780c3cd0f3b
Maroof <maroofsofi99@gmail.com>
2023-05-30 18:34:37 +0530

    removed heading

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = ()=>{
-    return <div>Hello world</div>;
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading/>);
+root.render();
